800fb09c970212ff08fa27b8929882c592e32d07
BestDemain <bestdemain@gmail.com>
2025-05-09 08:07:14 +0800

    Initial bat

---
+++ 前端启动.bat
@@ -0,0 +1,5 @@
+@echo off
+D:
+cd /d D:\Vscode\Project\VisualAnalytics\EduAssistSys\frontend
+set NODE_OPTIONS=--openssl-legacy-provider
+npm start
---
+++ 后端启动.bat
@@ -0,0 +1,6 @@
+@echo off
+call "C:\Users\90974\anaconda3\Scripts\activate.bat"
+call conda activate MyEnv
+D:
+cd D:\Vscode\Project\VisualAnalytics\EduAssistSys\backend
+python app.py
